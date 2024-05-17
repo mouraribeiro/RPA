61cf2725a5ccf83f27aba7b97d7d99b31a96ddb4
Rayanne Pacto <rayanne.ribeiro@pactonet.com.br>
2024-05-17 16:13:10 -0300

    teste de tela tkinter e executável

---
+++ olamundo.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+janela = Tk()
+
+
+janela.title("Interface")
+
+janela.geometry("400x240")
+
+instrucao = Label(text="\nOlá, Mundo!", font='Arial 30')
+instrucao.pack()
+
+
+janela.mainloop()
